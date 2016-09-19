c74852df0bbe193eb843551a1908997b9ad77ed4
edo_m18 <edo.m18@gmail.com>
2016-09-19 16:30:45 +0900

    update the scene.

--- Assets/Scripts/DaggerDebug.cs
+++ Assets/Scripts/DaggerDebug.cs
@@ -5,6 +5,11 @@ public class DaggerDebug : MonoBehaviour
 {
     public GameObject Target;
 
+    [SerializeField]
+    FunnelDagger _funnelDagger;
+
+    bool _isTargetting = false;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -15,8 +20,18 @@ public class DaggerDebug : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.T))
         {
-            var newPos = Vector3.MoveTowards(Target.transform.position, Vector3.zero, Time.deltaTime);
-            Target.transform.position = newPos;
+            if (_isTargetting)
+            {
+                _isTargetting = false;
+                _funnelDagger.SetTarget(null);
+            }
+            else
+            {
+                _isTargetting = true;
+                _funnelDagger.SetTarget(Target);
+            }
+            //var newPos = Vector3.MoveTowards(Target.transform.position, Vector3.zero, Time.deltaTime);
+            //Target.transform.position = newPos;
         }
 	}
 }
